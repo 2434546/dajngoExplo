5d21277f290aa6fe6de24e10a2ff9d3898f59337
2434546 <2434546@etu.cchic.ca>
2024-10-03 18:04:44 -0400

    version 2

--- demo_project/demo_app/urls.py
+++ demo_project/demo_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view.as_view(), name="index"),
+    path('login/', views.login_view.as_view(), name="login"),
+    path('reservations/', views.reservations_view.as_view(), name="reservations"),
+    path('voitures/', views.liste_voitures_view.as_view(), name="liste_voitures"),
 ]
--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -7,3 +7,15 @@ from django.views.generic.base import View
 class index_view(View):
     def get(self, request):
         return render(request, 'index.html')
+
+class login_view(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+class reservations_view(View):
+    def get(self, request):
+        return render(request, 'reservations.html')
+
+class liste_voitures_view(View):
+    def get(self, request):
+        return render(request, 'liste_voitures.html')
